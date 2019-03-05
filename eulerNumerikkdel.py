a8b170f9de9ad93c0984351b9347ee3b31da49eb
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2019-03-05 19:15:20 +0100

    Satte opp luftmotstand, plots fungerer nå greit

--- eulerNumerikkdel.py
+++ eulerNumerikkdel.py
@@ -3,8 +3,8 @@ import numpy as np
 from math import ceil
 
 
-def simulate(time_interval, dt, x0, v0):
-    filename = "track.txt"
+def simulate(time_interval, dt, x0, v0, k=0):
+    filename = "FUCK.txt"
     p = iptrack(filename)
     n = ceil(time_interval/dt)
     x = np.zeros(n + 1)
@@ -12,11 +12,12 @@ def simulate(time_interval, dt, x0, v0):
     t = np.zeros(n + 1)
     vx = v0
     x[0] = x0
+    y[0] = get_y(p, x[0])
     for i in range(n):
         ax = calculate_acceleration(p, x[i])
-        vx += ax*dt
+        vx += ax*dt - k*vx*dt
         x[i+1] = x[i] + vx*dt
-        y[i+1] = get_y(p, x[i])
+        y[i+1] = get_y(p, x[i+1])
         t[i+1] = t[i] + dt
     plt.plot(t, x)
     plt.xlabel('t')
@@ -52,7 +53,22 @@ def iptrack(filename):
 def calculate_acceleration(p, x):
     g = 9.81
     a = g*np.sin(alpha(p, x))*5/7
-    ax = a*np.sin(alpha(p, x))
-    return ax
+    return a
 
-simulate(100,0.01,1,0)
+
+def plot_bane(filename, minx, maxx, step):
+    n = ceil((maxx-minx)/step)
+    p = iptrack(filename)
+    y = np.zeros(n+1)
+    x = np.zeros(n+1)
+    x[0] = minx
+    y[0] = np.polyval(p, x[0])
+    for i in range(n):
+        x[i+1] = x[i] + step
+        y[i+1] = get_y(p, x[i+1])
+    plt.plot(x, y)
+    plt.show()
+
+
+#simulate(10, 0.01, 0.4, 0, 0.2)
+plot_bane("FUCK.txt", -0.7, 0.7, 0.01)
