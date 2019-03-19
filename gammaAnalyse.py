88a2dbe2b6599f863a42198407c01d58ce91ea7c
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2019-03-19 17:41:02 +0100

    La til data-plot i gamma-estimatene

--- gammaAnalyse.py
+++ gammaAnalyse.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 class DataPoint:
     def __init__(self, y, t):
@@ -22,15 +22,19 @@ def create_data_table(folder, fileNames):
 def gamma_estimates(data_table):
     estimates = np.zeros(len(data_table))
     variances = np.zeros(len(data_table))
+    all_g = np.zeros(sum([len(data) - 1 for data in data_table]))
+    all_index = 0
     for i in range(len(data_table)):
         wip = np.zeros(len(data_table[i])-1)
         for j in range(len(data_table[i])-1):
             pre = data_table[i][j]
             post = data_table[i][j+1]
             wip[j] = (pre.y - post.y)/(pre.y * (post.t - pre.t))
+            all_g[all_index] = wip[j]
+            all_index += 1
         estimates[i] = np.average(wip)
         variances[i] = np.var(wip, ddof=1)
-    return estimates, variances
+    return estimates, variances, all_g
 
 
 def refined_estimate(estimates, variances):
@@ -47,11 +51,17 @@ def stderr(var, n):
     return np.sqrt(var/n)
 
 
+def plotData(dataset):
+    plt.plot(dataset)
+    plt.show()
+
 data = create_data_table('data', ['P1120186.txt', 'P1120191.txt', 'P1120192.txt', '183.txt', '184.txt', '185.txt',
                                   'ONE.txt', 'TWO.txt', 'THREE.txt', 'FOUR.txt'])
-estimates, variances = gamma_estimates(data)
+estimates, variances, all_gammas = gamma_estimates(data)
 print('Averages:', estimates, '\nVariances:', variances)
 refined_avg, refined_var = refined_estimate(estimates, variances)
 print('Refined avg:', refined_avg, '\nRefined var:', refined_var, '\nStandardavvik: ', sd(refined_var))
 standardfeil = stderr(refined_var, len(data))
 print('Standard error:', standardfeil)
+
+plotData(all_gammas)
