89546adba9366ad7ad4fe893a8824d8492644b98
MittBrukernavn <mittbrukernavn.aa@gmail.com>
2019-04-02 19:08:40 +0200

    Gjorde grafene finere. Lagret dem i mappe med bilder.

--- eulerNumerikkdel.py
+++ eulerNumerikkdel.py
@@ -17,7 +17,7 @@ def simulate(time_interval, dt, x0, k=0.0):
     x[0] = x0
     y[0] = get_y(p, x[0])
     vx[0] = 0
-    f[0], N[0] = f_and_n(x[0], vx[0], calculate_acceleration(p, x[0], vx[0], k), p)
+    f[0], N[0] = f_and_n(x[0], vx[0], base_a(p, x[0]), p)
     for i in range(n):
         ax = calculate_acceleration(p, x[i], vx[i], k)
         vx[i+1] = vx[i] + ax*dt
@@ -25,48 +25,55 @@ def simulate(time_interval, dt, x0, k=0.0):
         y[i+1] = get_y(p, x[i+1])
         t[i+1] = t[i] + dt
         vy[i + 1] = compute_vy(p, vx[i + 1], x[i + 1])
-        f[i+1], N[i+1] = f_and_n(x[i+1], vx[i+1], ax, p)
+        f[i+1], N[i+1] = f_and_n(x[i+1], vx[i+1], base_a(p, x[i+1]), p)
     # eksempelplot:
     # m = 958
     t_192, x_192, y_192, vx_192, vy_192 = parse_track('192-autotrack.txt')
+    labelsize = 15
+    titlesize = 20
     func_i_dont_use, (xplot, yplot) = plt.subplots(2)
     xplot.plot(t, x)
     xplot.plot(t_192, x_192)
-    xplot.set_ylabel('x / m')
-    yplot.set_xlabel('t / s')
-    xplot.set_title('x(t)')
+    xplot.legend(['Numerisk', 'Eksperimentell'], loc='upper right')
+    xplot.set_ylabel('x / m', fontsize=labelsize)
+    yplot.set_xlabel('t / s', fontsize=labelsize)
+    xplot.set_title('x(t)', fontsize=titlesize)
     yplot.plot(t, y)
     yplot.plot(t_192, y_192)
-    yplot.set_xlabel('t / s')
-    yplot.set_ylabel('y / m')
-    yplot.set_title('y(t)')
+    yplot.legend(['Numerisk', 'Eksperimentell'], loc='upper right')
+    yplot.set_xlabel('t / s', fontsize=labelsize)
+    yplot.set_ylabel('y / m', fontsize=labelsize)
+    yplot.set_title('y(t)', fontsize=titlesize)
     plt.show()
     gunc_i_dont_use, (vxplot, vyplot) = plt.subplots(2)
-    vxplot.set_title('vx(t)')
-    vxplot.set_xlabel('t / s')
-    vxplot.set_ylabel('vx / (m/s)')
+    vxplot.set_title('vx(t)', fontsize=titlesize)
+    vxplot.set_xlabel('t / s', fontsize=labelsize)
+    vxplot.set_ylabel('vx / (m/s)', fontsize=labelsize)
     vxplot.plot(t, vx)
     vxplot.plot(t_192, vx_192)
-    vyplot.set_title('vy(t)')
-    vyplot.set_xlabel('t / s')
-    vyplot.set_ylabel('vy / (m/s)')
+    vxplot.legend(['Numerisk', 'Eksperimentell'], loc='upper right')
+    vyplot.set_title('vy(t)', fontsize=titlesize)
+    vyplot.set_xlabel('t / s', fontsize=labelsize)
+    vyplot.set_ylabel('vy / (m/s)', fontsize=labelsize)
     vyplot.plot(t, vy)
     vyplot.plot(t_192, vy_192)
+    vyplot.legend(['Numerisk', 'Eksperimentell'], loc='upper right')
     plt.show()
-    plt.plot(t, f)
-    plt.title('friksjon')
-    plt.xlabel('tid / s')
-    plt.ylabel ('(f/m) / (m/s^2) ')
+    plt.plot(x, f)
+    plt.title('Friksjonsakselerasjon', fontsize=titlesize)
+    plt.xlabel('x-posisjon / m', fontsize=labelsize)
+    plt.ylabel('(f/m) / (m/s^2) ', fontsize=labelsize)
     plt.show()
-    plt.plot(t, N)
-    plt.title('Normalkraft')
-    plt.xlabel('tid / s')
-    plt.ylabel('(N/m) / (m/s^2) ')
+    plt.plot(x, N)
+    plt.title('Normalakselerasjon', fontsize=titlesize)
+    plt.xlabel('x-posisjon / m', fontsize=labelsize)
+    plt.ylabel('(N/m) / (m/s^2)', fontsize=labelsize)
     plt.show()
 
 
 def f_and_n(x, v, a, p):
     return 9.81*np.sin(alpha(p, x)) - a, ((v**2)/R(p, x)) + 9.81*np.cos(x)
+
 
 def compute_vy(p, vx, x):
     dp = np.polyder(p)
@@ -95,6 +102,7 @@ def parse_track(filename):
     vx_slow.append((x_slow[-1] - x_slow[-2]) / (t_slow[-1] - t_slow[-2]))
     vy_slow.append((y_slow[-1] - y_slow[-2]) / (t_slow[-1] - t_slow[-2]))
     return np.array(t_slow), np.array(x_slow), np.array(y_slow), np.array(vx_slow), np.array(vy_slow)
+
 
 def trvalues(p, x):
     y = np.polyval(p, x)
@@ -125,9 +133,12 @@ def iptrack(filename):
 
 
 def calculate_acceleration(p, x, vx, k):
-    g = 9.81
-    a = g*np.sin(alpha(p, x))*5/7 - k*vx
+    a = base_a(p, x) - k*vx
     return a
+
+
+def base_a(p, x):
+    return 9.81*np.sin(alpha(p, x)) * (5/7)
 
 
 def plot_bane(filename, minx, maxx, step):
@@ -148,4 +159,4 @@ def plot_bane(filename, minx, maxx, step):
 
 simulate(9.57, 0.01, -0.585, 0.05)
 # blå er numerisk, oransje er autotracket
-plot_bane("banetrack.txt", -0.7, 0.7, 0.01)
+# plot_bane("banetrack.txt", -0.7, 0.7, 0.01)
